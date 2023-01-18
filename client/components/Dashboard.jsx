ccf14f96b15c873e8b81e9f041896f823c995c7c
Kun Ok Noh <bnohcub@hotmail.com>
2023-01-18 14:48:50 -0800

    getting API locations to render on page

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 import LocationSearch from './LocationSearch.jsx';
-import MapContainer from './MapContainer.jsx'
+import MapContainer from './MapContainer.jsx';
 
 const Dashboard = () => {
-  return (
-    <div className='p-20'>
-      <LocationSearch />
-    </div>
+  const [location, setLocation] = useState([]);
+  const searchCoffeeShops = async (location) => {
+    try {
+      console.log(location)
+      const response = await axios.get(`http://localhost:3001/search?location=${location}&categories=coffee`,
+        {
+          headers: {
+            Authorization: `Bearer KqmdPNyHvBAM9NyYYvYAqnnw0N367FN1UJ2vzgeDwVIKWVlFVr6XKDCmE-_xCP7UUkJzqehTpWjI5hm4NT3DwJ9kEDi7WDxeqU_Fc9Dlv0sTRGhBx3Q2kEcHUDHHY3Yx`,
+          },
+        }
+      );
+      console.log(response.data.businesses);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  )
-}
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      {/* <LocationSearch /> */}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          searchCoffeeShops(location);
+        }}
+      >
+        <input onChange={(e) => setLocation(e.target.value)} />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
 
 export default Dashboard;
